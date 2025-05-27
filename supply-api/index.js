5b83d0509d474582c446c1588bcdd16400b6b674
NITRO_OG <137004224+Fearlessff@users.noreply.github.com>
2025-05-27 16:08:05 +0100

    Add files via upload

---
+++ supply-api/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const circulatingSupply = 1000000000;
+const totalSupply = 999302618;
+
+app.get('/api/total-supply', (req, res) => {
+  res.json(totalSupply);
+});
+
+app.get('/api/circulating-supply', (req, res) => {
+  res.json(circulatingSupply);
+});
+
+app.listen(port, () => {
+  console.log(`Supply API running on http://localhost:${port}`);
+});
